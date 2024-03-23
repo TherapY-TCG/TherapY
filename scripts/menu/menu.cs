96494ed385173fc80a8e8f07bafe89e23400f74c
Maxime <maximechagnon00@gmail.com>
2024-03-23 13:22:23 +0100

    feat: exit button

---
+++ scripts/menu/menu.cs
@@ -0,0 +1,20 @@
+using Godot;
+using System;
+
+public partial class menu : Control {
+    public void onPlayPressed() {
+        // TODO
+    }
+
+    public void onCollectionPressed() {
+        // TODO
+    }
+
+    public void onSettingsPressed() {
+        // TODO
+    }
+
+    public void onExitPressed() {
+        GetTree().Quit();
+    }
+}
